d469bb4f489448dafce1a99032db04814add4bc1
rajurangoji <your-email@example.com>
2025-01-11 11:28:38 +0530

    changed the intro

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="max-w-5xl mx-auto">
       <h1 className="text-3xl font-bold underline text-center">
-        Hello Seller central
+        hello Agraharam
       </h1>
     </div>
   )
