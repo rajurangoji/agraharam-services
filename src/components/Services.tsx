3f2a637b98167d363a35a84bd8e985f04f462ee5
rajurangoji <your-email@example.com>
2025-02-15 21:58:56 +0530

    added new images and changed the code

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import Image_unknown3 from "../assets/Unknown-3.jpg";
-import Image_unknown4 from "../assets/Unknown-4.jpg";
-import Image_unknown8 from "../assets/Unknown-8.jpg";
-import bg_img from "../assets/bg-img.png";
-import galaxy from "../assets/galaxy-planet-.webp";
+import horoscope from '../assets/horoscope.jpg';
+import Donations from '../assets/Donations.jpg';
+import birth_chart from '../assets/birth_chart.jpg';
+import Catering from '../assets/catring.jpg';
+import education from '../assets/education.jpg'
+
 
 const services = [
   {
@@ -12,35 +13,35 @@ const services = [
     title: "Horoscope & Predictions",
     description:
       "Daily, weekly, and monthly horoscopes tailored to individual zodiac signs. Personalized predictions based on your birth chart.",
-    image: Image_unknown3,
+    image: horoscope,
   },
   {
     id: "2",
     title: "Birth Chart Generation",
     description:
       "Creation of detailed birth charts using your birth date, time, and location. Insights into personality traits and life paths based on astrological positions.",
-    image: Image_unknown4,
+    image: birth_chart,
   },
   {
     id: "3",
     title: "Remedies and Pariharas",
     description:
       "Recommendations for remedies to mitigate negative influences based on astrology. Suggestions for poojas, donations, and other spiritual practices.",
-    image: Image_unknown8,
+    image: Donations,
   },
   {
     id: "4",
     title: "Brahmin Catering Services",
     description:
       "Catering for special events such as weddings and housewarming ceremonies, provided by Brahmin chefs. Options for traditional dishes and services tailored to cultural practices.",
-    image: bg_img,
+    image: Catering,
   },
   {
     id: "5",
     title: "Educational Resources",
     description:
       "Access to articles, videos, and guides about astrology, zodiac signs, and horoscopes. Information on compatibility analyses and career guidance.",
-    image: galaxy,
+    image: education,
   },
 ];
 
